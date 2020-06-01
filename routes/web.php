872c979bc14e1b07b5f65afbcd42f4285966ff5d
heitordeve <heitor.developer@outlook.com>
2020-05-31 21:44:27 -0300

    ► Dev ◄ Aprendendo Rotas Redirect e View

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::redirect('redirect1', 'redirect2');
+
+Route::view('/view', 'welcome');
+
+// Route::get('redirect1', function () {
+//     return redirect('/redirect2');
+// });
+
+Route::get('redirect2', function () {
+    return "Redirect 02";
+});
+
 Route::get('/produtos/{idProduct?}', function ($idProduct = '') {
     return "Produtos {$idProduct}";
 });
