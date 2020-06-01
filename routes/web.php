648e421d8f46d40bce8cd75b54495618a4d1f240
heitordeve <heitor.developer@outlook.com>
2020-05-31 21:33:58 -0300

    ► Dev ◄ Rotas com parâmetros

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/produtos/{idProduct?}', function ($idProduct = '') {
+    return "Produtos {$idProduct}";
+});
+
+Route::get('/categoria/{flag}/posts', function ($flag) {
+    return "Posts da Categoria: {$flag}";
+});
+
+Route::get('/categorias/{flag}', function ($flag) {
+    return "Produtos da Categoria: {$flag}";
+});
+
 Route::any('/any', function () {
     return 'Any';
 });
